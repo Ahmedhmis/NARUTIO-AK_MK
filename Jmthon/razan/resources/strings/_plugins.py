ea7760aba40b63a8f3ee19965a347529b9758001
- Muhammad ALZUHAIRY <87212030+RR9R7@users.noreply.github.com>
2021-09-21 22:09:29 +0300

    Update _plugins.py

--- Jmthon/razan/resources/strings/_plugins.py
+++ Jmthon/razan/resources/strings/_plugins.py
@@ -1,4 +1,63 @@
+import os
+import re
+import time
+import heroku3
+import lottie
+import requests
+from telethon import version
+from telethon import events
+
 from userbot import * 
+from userbot.Config import *
+from userbot.helpers import *
 
 RAZAN = "**▾∮ يجب وضع اسم الاغنية اولا للبحث عنها", 15**"
-rozrtba = "مطـور السـورس 🧸🤍" if user_id == ROZDEV else ("عضـو عادي 🤍")
+
+async def roza():
+    _, check_sgnirts = check_data_base_heal_th()
+    sudo = "Enabled" if Config.SUDO_USERS else "Disabled"
+    uptime = await get_readable_time((time.time() - StartTime))
+    try:
+        useragent = (
+            "Mozilla/5.0 (Linux; Android 10; SM-G975F) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/80.0.3987.149 Mobile Safari/537.36"
+        )
+        user_id = Heroku.account().id
+        headers = {
+            "User-Agent": useragent,
+            "Authorization": f"Bearer {Config.HEROKU_API_KEY}",
+            "Accept": "application/vnd.heroku+json; version=3.account-quotas",
+        }
+        path = "/accounts/" + user_id + "/actions/get-quota"
+        r = requests.get(heroku_api + path, headers=headers)
+        result = r.json()
+        quota = result["account_quota"]
+        quota_used = result["quota_used"]
+
+        # Used
+        remaining_quota = quota - quota_used
+        math.floor(remaining_quota / quota * 100)
+        minutes_remaining = remaining_quota / 60
+        hours = math.floor(minutes_remaining / 60)
+        minutes = math.floor(minutes_remaining % 60)
+        # Current
+        App = result["apps"]
+        try:
+            App[0]["quota_used"]
+        except IndexError:
+            AppQuotaUsed = 0
+        else:
+            AppQuotaUsed = App[0]["quota_used"] / 60
+            math.floor(App[0]["quota_used"] * 100 / quota)
+        AppHours = math.floor(AppQuotaUsed / 60)
+        AppMinutes = math.floor(AppQuotaUsed % 60)
+        dyno = f"{AppHours}h {AppMinutes}m/{hours}h {minutes}m"
+    except Exception as e:
+        dyno = e
+    return f"**▾∮ معلومات جمثـون ثـون***\
+                 \n - قاعده البيانات : تعمـل بنجـاح\
+                  \n - سودو : {sudo}\
+                  \n - مدة التشغيل : {uptime}\
+                  \n - مده الاستخدام : {dyno}\
+                  "
