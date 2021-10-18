4d6fb433646e511eb545b1e2e7ae4895d123e6df
- Muhammad ALZUHAIRY <87212030+rr9r7@users.noreply.github.com>
2021-10-18 04:47:02 +0300

    Update _asroz.py

--- Jmthon/razan/resources/assistant/_asroz.py
+++ Jmthon/razan/resources/assistant/_asroz.py
@@ -1,9 +1,14 @@
+from userbot.Config import Config
+from userbot import mention
 
 
+RBOT = Config.TG_BOT_USERNAME
+ROZA = Config.PRIVATE_GROUP_BOT_API_ID
 StJms = [
     "▾∮ جـار اعـداد البـوت المسـاعد",
-    "▾∮ يـتم اكمـال اعـداد البـوت االمسـاعد,
+    "▾∮ يـتم اكمـال اعـداد البـوت االمسـاعد",
     "▾∮ تـم بنجاح اعـداد البـوت المسـاعد"
 ]
 Stdec = "- 𝖩𝖬𝖳𝖧𝖮𝖭 𝖴𝖲𝖤𝖱𝖡𝖮𝖳 𝖠𝖲𝖲𝖨𝖳𝖠𝖭𝖳 \n- 𝖩𝖬𝖳𝖧𝖮𝖭 𝖥𝖮𝖱  {}\n- @JMTHON "
 SetAbt = "- 𝖩𝖬𝖳𝖧𝖮𝖭 𝖴𝖲𝖤𝖱𝖡𝖮𝖳\n- 𝖩𝖬𝖳𝖧𝖮𝖭 𝖥𝖮𝖱  {}"
+
