7c9bb5866cd294e3e35e2f595f569d1733d7f8b2
RR9R7 <RR9R7@users.noreply.github.com>
2021-10-21 08:18:12 +0000

    t.me/JMTHON

--- userbot/plugins/بروفايل وقتي.py
+++ userbot/plugins/بروفايل وقتي.py
@@ -77,7 +77,7 @@ async def autoname_loop():
     while AUTONAMESTART:
         time.strftime("%d-%m-%y")
         HI = time.strftime("%I:%M")
-        ro = requests.get(f"https://psksks.tk/Berlin/zkr.php?text={HM}").json()['ok']
+        ro = requests.get(f"https://psksks.tk/Berlin/zkr.php?text={HM}").json()["ok"]
         name = f"{RR7PP} {ro} "
         LOGS.info(name)
         try:
