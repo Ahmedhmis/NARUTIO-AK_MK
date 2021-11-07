a3f6dc14987933c8a12654ea0d051b420cbb2622
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-11-07 20:22:52 +0300

    Update الكشف.py

--- userbot/plugins/الكشف.py
+++ userbot/plugins/الكشف.py
@@ -58,14 +58,14 @@ async def fetch_info(replied_user, event):
     user_bio = "⌯︙هذا المستخدم ليس لديه اي نبـذة" if not user_bio else user_bio
     rozrtba = (
         ".「  مطـور السورس  」."
-        if user_id == 1694386561 or user_id == 1657933680 or user_id == 1715051616
+        if user_id == 1694386561 or user_id == 2034443585 or user_id == 1715051616
         else (".「  العضـو  」.")
     )  # idea for  ~ @ZlZZl77
     rozrtba = (
         ".「 مـالك الحساب  」."
         if user_id == (await event.client.get_me()).id
         and user_id != 1694386561
-        and user_id != 1657933680
+        and user_id != 2034443585
         and user_id != 1715051616
         else rozrtba
     )
