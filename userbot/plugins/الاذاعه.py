b7f930762dfd03eed4553f04e6616c88d603d0d5
RR9R7 <RR9R7@users.noreply.github.com>
2021-11-09 13:04:49 +0000

    t.me/JMTHON

--- userbot/plugins/الاذاعه.py
+++ userbot/plugins/الاذاعه.py
@@ -1,45 +1,47 @@
-from userbot import jmthon
-from telethon import events
 import asyncio
+
 from userbot import jmthon
+
 from ..core.managers import edit_or_reply
 from . import *
 
 plugin_category = "utils"
 
+
 @jmthon.on(admin_cmd(pattern="وجه ?(.*)"))
 async def srzo(event):
     if not event.is_reply:
-        return await event.edit('- يجـب الرد على الرسـالة اولا')
-    
+        return await event.edit("- يجـب الرد على الرسـالة اولا")
+
     surze = event.pattern_match.group(1)
-    if (surze == ''):
+    if surze == "":
         surze = 1.5
     else:
         try:
             surze = float(surze)
         except:
             surze = 1.5
-    await event.edit(f'**{surze} مـن الثواني**')
+    await event.edit(f"**{surze} مـن الثواني**")
     mrzo = await event.get_reply_message()
-    await event.edit('يتـم التعـرف عـلى المجمـوعات')
+    await event.edit("يتـم التعـرف عـلى المجمـوعات")
     muhgr = await event.client.get_dialogs()
-    await event.edit(f'{len(muhgr)} تم العثور على الدردشات يتم تحديد المجموعات.')
-    
+    await event.edit(f"{len(muhgr)} تم العثور على الدردشات يتم تحديد المجموعات.")
+
     i = 0
     for grup in muhgr:
         if grup.is_group:
-            await event.edit(f'{grup.name}  يتـم ارسـال الرسـالة الخـاصة بـك انتـظر...')
+            await event.edit(f"{grup.name}  يتـم ارسـال الرسـالة الخـاصة بـك انتـظر...")
             try:
                 await grup.send_message(mrzo)
             except:
-                await event.edit(f'- لا يمكن إرسال رسالتك الى **{grup.name}** ')
+                await event.edit(f"- لا يمكن إرسال رسالتك الى **{grup.name}** ")
                 await asyncio.sleep(surze)
                 continue
             i += 1
-            await event.edit(f'- يتم الارسال الى {grup.name}')
+            await event.edit(f"- يتم الارسال الى {grup.name}")
             await asyncio.sleep(surze)
-    await event.edit(f'تم الارسال بنجاح الى {i} من المجموعات بنجاح ✓')
+    await event.edit(f"تم الارسال بنجاح الى {i} من المجموعات بنجاح ✓")
+
 
 @jmthon.ar_cmd(
     pattern="حول ?(.*)$",
