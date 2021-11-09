a48f80769263cd05f67bcb0af58efe6e0580c580
- Muhammad ALZUHAIRY <87212030+rr9r7@users.noreply.github.com>
2021-11-09 18:43:28 +0300

    Update session.py

--- userbot/core/session.py
+++ userbot/core/session.py
@@ -13,7 +13,7 @@ loop = None
 if Config.STRING_SESSION:
     session = StringSession(str(Config.STRING_SESSION))
 else:
-    session = "catuserbot"
+    session = "jmthon"
 
 try:
     jmthon = CatUserBotClient(
