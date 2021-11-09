57463fa3a2e759eac3c4547556a16b213e82ae69
- Muhammad ALZUHAIRY <87212030+rr9r7@users.noreply.github.com>
2021-11-09 14:01:20 +0300

    Update الاذاعه.py

--- userbot/plugins/الاذاعه.py
+++ userbot/plugins/الاذاعه.py
@@ -5,35 +5,6 @@ from . import *
 
 plugin_category = "utils"
 
-
-@jmthon.ar_cmd(
-    pattern="وجه ?(.*)$",
-    command=("وجه", plugin_category),
-)
-async def gcast(event):
-    if not event.out and not is_fullsudo(event.sender_id):
-        return await edit_or_reply(event, "هـذا الامـر مقـيد للسـودو")
-    xx = event.pattern_match.group(1)
-    if not xx:
-        return edit_or_reply(event, "** ⌯︙يجـب وضـع نـص مع الامـر للتوجيـه**")
-    tt = event.text
-    msg = tt[6:]
-    event = await edit_or_reply(event, "** ⌯︙يتـم الـتوجيـة للـمجموعـات انتـظر قليلا**")
-    er = 0
-    done = 0
-    async for x in bot.iter_dialogs():
-        if x.is_group:
-            chat = x.id
-            try:
-                done += 1
-                await bot.send_message(chat, msg)
-            except BaseException:
-                er += 1
-    await event.edit(
-        f"تـم بنـجـاح فـي {done} من الـدردشـات , خطـأ فـي {er} من الـدردشـات"
-    )
-
-
 @jmthon.ar_cmd(
     pattern="حول ?(.*)$",
     command=("حول", plugin_category),
