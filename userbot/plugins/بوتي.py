3a010b545770338b5eb5d139f7fa9c0b0b787999
RR9R7 <RR9R7@users.noreply.github.com>
2021-11-12 11:00:56 +0000

    t.me/JMTHON

--- userbot/plugins/بوتي.py
+++ userbot/plugins/بوتي.py
@@ -1,16 +1,15 @@
-import asyncio
-from userbot.utils import admin_cmd
 from userbot import jmthon
+from userbot.utils import admin_cmd
+
 from ..Config import Config
+
 
 @jmthon.on(admin_cmd(pattern="بوتي$"))
 async def _(event):
     if event.fwd_from:
         return
     TG_BOT_USERNAME = Config.TG_BOT_USERNAME
-    await event.reply(
-        f"**- البوت الخاص بك هو** \n {TG_BOT_USERNAME}"
-    )
+    await event.reply(f"**- البوت الخاص بك هو** \n {TG_BOT_USERNAME}")
 
 
 # حتى هذا تخمطه  😂؟
