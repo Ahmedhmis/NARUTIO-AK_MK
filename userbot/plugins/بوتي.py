74200028fb56624151d41deb134c93accba4eaa1
RR9R7 <RR9R7@users.noreply.github.com>
2021-11-12 10:03:26 +0000

    t.me/JMTHON

--- userbot/plugins/بوتي.py
+++ userbot/plugins/بوتي.py
@@ -1,10 +1,14 @@
 import os
+
+from userbot import jmthon
+
 from ..utils import admin_cmd
 from . import *
-from userbot import jmthon
+
 
 @jmthon.on(admin_cmd("بوتي$", incoming=True))
 async def proz(event):
-  msg = await bot.send_message(event.chat_id, str(os.environ.get("TG_BOT_USERNAME")))
+    await bot.send_message(event.chat_id, str(os.environ.get("TG_BOT_USERNAME")))
 
-#حتى هذا تخمطه  😂؟ 
+
+# حتى هذا تخمطه  😂؟
