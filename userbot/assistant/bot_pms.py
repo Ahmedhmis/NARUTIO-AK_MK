95c807a7161421f3453d481ee130ade25cab5f64
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2021-11-16 01:50:33 +0300

    Update bot_pms.py

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -1,3 +1,11 @@
+# @Jmthon - < https://t.me/Jmthon >
+# Copyright (C) 2021 - JMTHON-AR
+# All rights reserved.
+#
+# This file is a part of < https://github.com/JMTHON-AR/JMTHON >
+# Please read the GNU Affero General Public License in;
+# < https://github.com/JMTHON-AR/JM-THON/blob/master/LICENSE
+# ===============================================================
 import re
 from collections import defaultdict
 from datetime import datetime
