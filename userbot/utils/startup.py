a7c59d4c0f0810f78cdf75bdd247154b00a70016
- Muhammad ALZUHAIRY <87212030+rr9r7@users.noreply.github.com>
2021-12-18 11:59:14 +0300

    Update startup.py

--- userbot/utils/startup.py
+++ userbot/utils/startup.py
@@ -201,7 +201,7 @@ async def autojo():
 
 async def autozs():
     try:
-        await jmthon(JoinChannelRequest("@GroupJmthon"))
+        await jmthon(JoinChannelRequest("@RR7PP))
         if gvar("AUTOZS") is False:
             return
         else:
