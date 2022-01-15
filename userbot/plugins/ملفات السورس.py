c7775a7d308e508d8db5088b7c64ea3159b6d34a
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2022-01-15 15:15:19 +0300

    Update ملفات السورس.py

--- userbot/plugins/ملفات السورس.py
+++ userbot/plugins/ملفات السورس.py
@@ -22,22 +22,6 @@ async def _(event):
     await edit_or_reply(event, OUTPUT)
 
 
-@jmthon.ar_cmd(
-    pattern="فاراتي$",
-    command=("فاراتي", plugin_category),
-    info={
-        "header": "To list all environment values in userbot.",
-        "description": "to show all heroku vars/Config values in your userbot",
-        "usage": "{tr}env",
-    },
-)
-async def _(event):
-    "To show all config values in userbot"
-    cmd = "env"
-    o = (await _catutils.runcmd(cmd))[0]
-    OUTPUT = f"**[جـمـثـون](tg://need_update_for_some_feature/) قـائمـة الـفـارات:**\n\n\n{o}"
-    await edit_or_reply(event, OUTPUT)
-
 
 @jmthon.ar_cmd(
     pattern="متى$",
