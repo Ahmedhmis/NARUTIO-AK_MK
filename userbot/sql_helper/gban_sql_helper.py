800152c00579c54b3d1ec852d5b41e702b1ca74f
JMTHON <85131843+JMTHON-AR@users.noreply.github.com>
2022-01-23 21:02:28 +0300

    Update gban_sql_helper.py

--- userbot/sql_helper/gban_sql_helper.py
+++ userbot/sql_helper/gban_sql_helper.py
@@ -46,13 +46,13 @@ def get_gbanuser(chat_id):
         SESSION.close()
 
 
-def catgban(chat_id, reason):
+def rzgban(chat_id, reason):
     adder = GBan(str(chat_id), str(reason))
     SESSION.add(adder)
     SESSION.commit()
 
 
-def catungban(chat_id):
+def rozungban(chat_id):
     rem = SESSION.query(GBan).get(str(chat_id))
     if rem:
         SESSION.delete(rem)
